7a9d6f152b590c47d25eb9affb3dd3ee7608241d
Nial John <135521072+Dragon753nite@users.noreply.github.com>
2024-06-10 14:54:09 +0300

    Revert "Completed window resize detector"
    This reverts commit d7d949eb442ad103a2b02efbb24e4cda03f522b4.

--- scripts.js
+++
@@ -1,7 +0,0 @@
-window.onload = screen;
-window.onresize = screen;
-
-function screen(){
-    myWidth = window.innerWidth;
-    document.getElementById("size").innerHTML = "Width: " + myWidth + "px"
-}
